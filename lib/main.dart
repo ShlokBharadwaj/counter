0d2397513259536583fcfeaa4576a7c0c1bace2a
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-09-21 12:56:47 +0530

    Add: Valid and Invalid state

--- lib/main.dart
+++ lib/main.dart
@@ -25,3 +25,21 @@ class _HomePageState extends State<HomePage> {
     return Container();
   }
 }
+
+@immutable
+abstract class CounterState {
+  final int value;
+  const CounterState(this.value);
+}
+
+class CounterStateValid extends CounterState {
+  const CounterStateValid(int value) : super(value);
+}
+
+class CounterStateInvalidNumber extends CounterState {
+  final String invalidNumber;
+  const CounterStateInvalidNumber({
+    required this.invalidNumber,
+    required int previousValue,
+  }) : super(previousValue);
+}
